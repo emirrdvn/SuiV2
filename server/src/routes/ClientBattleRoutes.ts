030e3612024f7fb632246533eb2c70bca8e2b89f
Alihan Gündoğdu <alihangundogdu01@gmail.com>
2025-04-26 21:45:12 +0300

    c

--- server/src/routes/ClientBattleRoutes.ts
+++ server/src/routes/ClientBattleRoutes.ts
@@ -1,13 +1,24 @@
 import { Router, Request, Response } from 'express';
+import { saveJsonToFile } from '../FileWriter';
 
 const battleRouter = Router();
 
 battleRouter.get('/start/:dungeonId', (req: Request, res: Response) => {
     const dungeonId = req.params['dungeonId'];
+    const accountId = req.headers['address'] as string;
+
+    saveJsonToFile(accountId,{
+        dungeonId: dungeonId,
+        accountId: accountId,
+        startTime: new Date().toISOString(),
+        attacks: [],
+    });
+
+
     // Burada battle başlatma işlemleri yapılacak
     // Örnek olarak sadece dungeonId'yi döndürüyoruz
     // TODO burada json dosyası oluşuturp içine log kaydeilecek
-    res.json({ message: `Battle started for dungeon ID: ${dungeonId}` });
+    res.status(200).json({ message: `Battle started for dungeon ID: ${dungeonId}`});
 });
 
 battleRouter.get('/end/:dungeonId', (req: Request, res: Response) => {
